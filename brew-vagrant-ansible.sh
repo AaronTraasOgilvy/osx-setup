1b18a905e53e2a389067a8ab130d3c9253e014ad
Aaron Traas <aaron@traas.org>
2016-02-03 16:22:15 -0500

    Install homebrew/vagrant/ansible/virtualbox

---
+++ brew-vagrant-ansible.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# install homebrew
+which -s brew
+if [[ $? != 0 ]] ; then
+    # Install Homebrew
+    # https://github.com/mxcl/homebrew/wiki/installation
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+# install caskroom
+brew tap phinze/cask && brew install brew-cask
+
+brew update && brew upgrade
+
+brew install \
+    ansible \
+    ssh-copy-id
+
+brew cask install --appdir=/Applications \
+	vagrant \
+    virtualbox
